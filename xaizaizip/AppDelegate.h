b2d96408a773fe0468b3d12d84a733c2ebf50eab
wyzw <wyzw@wyzwdemacbook-pro.local>
2018-03-16 15:18:04 +0800

    Initial Commit

---
+++ xaizaizip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xaizaizip
+//
+//  Created by wyzw on 2018/3/16.
+//  Copyright © 2018年 wyzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xaizaizip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xaizaizip
+//
+//  Created by wyzw on 2018/3/16.
+//  Copyright © 2018年 wyzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
